6a3c2388b064d5bec357fd1557537fb5cd9d3b14
dxZhang <dxzhang49@gmail.com>
2023-05-02 00:12:22 +0800

    Rename markdown file

--- note.sh
+++ note.sh
@@ -10,24 +10,24 @@ msg=$2
 lang="cpp"
 code_file=$(basename $(find . -type f -maxdepth 1 -name "1.*.$lang"))
 md_file=$(echo $code_file | sed "s/\.$lang$/.md/")
-md_dir="notes/$md_file"
+md_path="notes/$md_file"
 
 echo $code_file
 echo $md_file
-echo $md_dir
+echo $md_path
 
 mkdir -p notes
 
-if [ ! -e $md_dir ]; then
-    touch $md_dir
-    echo "# ${code_file%.*}\n" > $md_dir
-    echo "Created new Markdown file $md_dir"
+if [ ! -e $md_path ]; then
+    touch $md_path
+    echo "# ${code_file%.*}\n" > $md_path
+    echo "Created new Markdown file $md_path"
 fi
 
-echo "## ${msg}\n" >> $md_dir
-echo "\`\`\` $lang" >> $md_dir
-cat  $code_file >> $md_dir
-echo "\`\`\`\n" >> $md_dir
-echo "" >> $md_dir
+echo "## ${msg}\n" >> $md_path
+echo "\`\`\` $lang" >> $md_path
+cat  $code_file >> $md_path
+echo "\`\`\`\n" >> $md_path
+echo "" >> $md_path
 
-echo "Updated $md_dir"
+echo "Updated $md_path"
